cf353a979ecdba235bc100e8f06cae6c8a9eb282
VieVie31 <orissermaroix@gmail.com>
2017-12-15 16:54:09 +0100

    clean code

---
+++ data.py
@@ -0,0 +1,60 @@
+import os
+import random
+import collections
+from glob import glob
+from tqdm import tqdm
+
+import numpy as np
+from skimage import io, transform
+from sklearn.model_selection import train_test_split
+
+
+def load(dir_path, im_size):
+    """Load a directory of images.
+    
+    Parameters
+    -------------
+    dir_path : str
+        Directory path. Structure is dir_path/classes/images
+    
+    im_size : tuple
+        Shape for each image (don't forget channels)
+    """
+    imlabel = lambda path: path.split('/')[-2]
+    
+    impath = glob.glob(dir_path + "/*/*png")
+    images = []
+    for i in tqdm.tqdm(impath):
+        f = transform.resize(io.imread(i), im_size)
+        images.append((f, imlabel(i))
+    return images
+
+
+def onehot_label(labels):
+    """Return dict {class_name: onehot_encoding}.
+    Keep labels sorted.
+    """"
+    l = sorted(set(labels))
+    r = {}
+    for i, c in enumerate(l):
+        t = [0] * len(l)
+        t[i] = 1
+        r[c] = t
+    return r
+
+
+def train_val_test_split(dataset, prc_test=0.2, prc_val=0, random_state=None):
+    x, y = dataset
+    X_train, Y_train,  X_test, Y_test = train_test_split(*dataset, test_size=prc_test, random_state=random_state)
+    
+    if prc_val:
+        X_train, Y_train, X_val, Y_val = train_test_split(X_train, Y_train, test_size=prc_val, random_state=random_state)
+        return (X_train, Y_train), (X_val, Y_val), (X_test, Y_test)
+    else:
+        return (X_train, Y_train), (X_test, Y_test)
+
+
+def normalize(data):
+    m = data.mean()
+    s = data.std()
+    return (data - m) / s, m, s
--- train.py
+++ train.py
@@ -1,185 +1,125 @@
-import keras
-import keras.backend as K
+import data
 
-import os
 
+
+# Now import real libs
 import numpy as np
 
-from tqdm import tqdm
-from glob import glob
-from random import shuffle
-from collections import Counter
+# Keras
+import keras
+import keras.backend as K
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Conv2D, Activation, Dropout, MaxPooling2D, GlobalMaxPooling2D, BatchNormalization, Flatten
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.optimizers import Adam
+from keras.optimizers import Adadelta
 from keras.preprocessing.image import ImageDataGenerator
 
-from skimage import transform, io
 
-from sklearn.utils import class_weight
+# Data
+im_size = (75, 75, 3)
 
+def preprocess_noval():
+    """
+    Returns
+    ------------
+    training_generator
+    test_generator
+    mean for normalization
+    std for normalization
+    """
+    dataset = data.load("../train")
+    x, y = dataset
+    x, m, s = data.normalize(x)
+    (x_train, y_train), (x_test, y_test) = data.train_val_test_split(x, y, prc_test=0.2)
 
-IM_SIZE = (75, 75, 3)
+    training_generator = ImageDataGenerator(
+            featurewise_center=False, 
+            samplewise_center=False, 
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False, 
+            rotation_range=180,
+            width_shift_range=.1,
+            height_shift_range=.1,
+            horizontal_flip=True,
+            vertical_flip=True,
+            zoom_range=0.2
+    )
 
-def imread(path):
-    return io.imread(path)
-
-def imlabel(path):
-    return path.split('/')[-2]
-
-L = []
-
-im_train = glob('../train/*/*.png')
-
-for im_path in tqdm(im_train):
-    feats = transform.resize(imread(im_path), IM_SIZE)
-    label = imlabel(im_path)
-    L.append((feats, label))
-
-x_data, x_label = zip(*L)
-
-#one hot encoding of labels
-classes = sorted(list(set(x_label)))
-
-def one_hot(c, n):
-    o = [0] * n
-    o[c] = 1
-    return o
-
-one_hot_encoder = {c: one_hot(i, len(classes)) for i, c in enumerate(classes)}
-
-x_label = np.array(list(map(lambda s: one_hot_encoder[s], x_label)))
-
-
-#split data
-data = list(zip(x_data, x_label))
-
-shuffle(data)
-
-x_data, x_label = zip(*data)
-
-x_train, x_test = x_data[:int(len(x_data) * .9)],  x_data[int(len(x_data) * .9):]
-y_train, y_test = x_label[:int(len(x_data) * .9)], x_label[int(len(x_data) * .9):]
-
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-
-x_test = np.array(x_test)
-y_test = np.array(y_test)
+    test_generator = ImageDataGenerator(
+            featurewise_center=False, 
+            samplewise_center=False, 
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False, 
+            rotation_range=180,
+            width_shift_range=.1,
+            height_shift_range=.1,
+            horizontal_flip=True,
+            vertical_flip=True,
+            zoom_range=0.2
+    )
+    return training_generator.flow(x_train, y_train), test_generator.flow(x_test, y_test), m, s
 
 
+# Build model
 from keras.applications import VGG16
+def build_model():
+	vgg = VGG16(
+	    input_shape=im_size,
+	    include_top=False,
+	    weights='imagenet'
+	)
 
-vgg = VGG16(
-    input_shape=(IM_SIZE[0], IM_SIZE[0], 3),
-    include_top=False,
-    weights='imagenet'
-)
+
+	partial_vgg = vgg.get_layer('block2_pool').output
+
+	model = Conv2D(64, (3, 3), activation='elu')(partial_vgg)
+	model = Conv2D(64, (3, 3), activation='elu')(model)
+	model = MaxPooling2D((2, 2))(model)
+
+	model = Conv2D(64, (3, 3), activation='elu')(model)
+	model = Conv2D(64, (3, 3), activation='elu')(model)
+	model = MaxPooling2D((2, 2))(model)
+
+	model = Flatten()(model)
+
+	model = Dropout(.25)(model)
+	model = Dense(len(classes), activation='softmax')(model)
+
+	model = Model(input=[vgg.input], output=model)
+
+	to_freeze = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2']
+	for t_f in to_freeze:
+	    model.get_layer(t_f).trainable = False
+	return model
 
 
-partial_vgg = vgg.get_layer('block2_pool').output
 
-model = Conv2D(64, (3, 3), activation='elu')(partial_vgg)
-model = Conv2D(64, (3, 3), activation='elu')(model)
-model = MaxPooling2D((2, 2))(model)
 
-model = Conv2D(64, (3, 3), activation='elu')(model)
-model = Conv2D(64, (3, 3), activation='elu')(model)
-model = MaxPooling2D((2, 2))(model)
-
-model = Flatten()(model)
-
-model = Dropout(.25)(model)
-model = Dense(len(classes), activation='softmax')(model)
-
-model = Model(input=[vgg.input], output=model)
-
-to_freeze = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2']
-for t_f in to_freeze:
-    model.get_layer(t_f).trainable = False
-
+# Compile
+opt = Adadelta()
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+# Prepare fit
+from sklearn.utils import class_weight
 
 check = ModelCheckpoint("weights.{epoch:02d}-{val_loss:.5f}.hdf5", monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto')
 early = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, mode='auto')
-
-print(model.summary())
-
-
-training_generator = ImageDataGenerator(
-        featurewise_center=False, 
-        samplewise_center=False, 
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False, 
-        rotation_range=180,
-        width_shift_range=.1,
-        height_shift_range=.1,
-        horizontal_flip=True,
-        vertical_flip=True,
-        zoom_range=0.2
-)
-
-validation_generator = ImageDataGenerator(
-        featurewise_center=False, 
-        samplewise_center=False, 
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False, 
-        rotation_range=180,
-        width_shift_range=.1,
-        height_shift_range=.1,
-        horizontal_flip=True,
-        vertical_flip=True,
-        zoom_range=0.2
-)
-
 cw = class_weight.compute_class_weight('balanced', np.unique(y_train.argmax(1)), y_train.argmax(1))
 
+batch_size = 32
+
+# Fit
 h = model.fit_generator(
     training_generator.flow(x_train, y_train),
     class_weight=cw,
-    steps_per_epoch=len(x_train) / 32,
+    steps_per_epoch=len(x_train) / batch_size,
     validation_data=validation_generator.flow(x_test, y_test),
-    validation_steps=len(x_test) / 32,
+    validation_steps=len(x_test) / batch_size,
     epochs=2000,
     callbacks=[early, check]
 )
 
 model.save("vgg_transfert_learning.h5")
-
-#plug a random forest on the CNN embeding
-model_embeding = model.layers[-3].output
-
-features_extractor = Model(input=[model.input], output=model_embeding)
-
-#make a syntethic dataset with data augmentation
-augmented_imgs, augmented_labels = [], []
-for i in tqdm(range(500)):
-    a, b = training_generator.flow(x_train, y_train).next()
-    augmented_imgs.extend(a)
-    augmented_labels.extend(b.argmax(1))
-
-augmented_imgs = np.array(augmented_imgs)
-augmented_labels = np.array(augmented_labels)
-
-augmented_feaures = features_extractor.predict(augmented_imgs)
-
-
-from sklearn.ensemble import RandomForestClassifier
-
-clf = RandomForestClassifier()
-clf.fit(augmented_feaures, augmented_labels)
-
-augmented_feaures = features_extractor.predict(x_test)
-
-
-
-
-
-
-
-
-
