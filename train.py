d81dc9a8832fb9750c1e1fe3e5eb0b500b73be69
Adrien Pouyet <adrien@pouyet.fr>
2017-12-18 19:08:22 +0100

    adding block4 and fintuing up to block3

--- train.py
+++ train.py
@@ -3,18 +3,15 @@ import data
 import numpy as np
 
 # Keras
-import keras
-import keras.backend as K
-
-from keras.models import Sequential, Model
-from keras.layers import Dense, Conv2D, Activation, Dropout, MaxPooling2D, GlobalMaxPooling2D, BatchNormalization, Flatten
+from keras.models import Model
+from keras.layers import Dense, Conv2D, Dropout, MaxPooling2D, Flatten, GlobalAveragePooling2D
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.optimizers import Adadelta
 from keras.preprocessing.image import ImageDataGenerator
-from skimage import transform, io
-IM_SIZE = (75, 75, 3)
+from keras.applications import VGG16
 
-import data
+im_size = (224, 224, 3)
+
 
 def preprocess():
     """
@@ -25,7 +22,7 @@ def preprocess():
     mean for normalization
     std for normalization
     """
-    dataset = data.load("../train", im_size)
+    dataset = data.load("./train", im_size)
     x, y = zip(*dataset)
     r = data.onehot_label(y)
     y = list(map(lambda k: r[k], y))
@@ -67,7 +64,6 @@ def preprocess():
 
 
 # Build model
-from keras.applications import VGG16
 def build_model():
     vgg = VGG16(
         input_shape=im_size,
@@ -75,8 +71,7 @@ def build_model():
         weights='imagenet'
     )
 
-
-    partial_vgg = vgg.get_layer('block3_pool').output
+    partial_vgg = vgg.get_layer('block4_pool').output
 
     model = Conv2D(64, (3, 3), activation='elu')(partial_vgg)
     model = Conv2D(64, (3, 3), activation='elu')(model)
@@ -84,45 +79,39 @@ def build_model():
 
     model = Conv2D(64, (3, 3), activation='elu')(model)
     model = Conv2D(64, (3, 3), activation='elu')(model)
-    model = MaxPooling2D((2, 2))(model)
-
-    model = Flatten()(model)
+    model = GlobalAveragePooling2D()(model)
 
     model = Dropout(.25)(model)
     model = Dense(12, activation='softmax')(model)
 
     model = Model(input=[vgg.input], output=model)
 
-    to_freeze = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2', 'block3_conv1', 'block3_conv2', 'block3_conv3'] #, 'block4_conv1', 'block4_conv2', 'block4_conv3']
+    to_freeze = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2']#, 'block3_conv1', 'block3_conv2', 'block3_conv3']
     for t_f in to_freeze:
         model.get_layer(t_f).trainable = False
     return model
 
 
 # Call functions
-model = build_model()
 train_gen, (x_train, y_train), test_gen, (x_test, y_test), mean, std = preprocess()
 y_train = np.asarray(y_train)
 y_test = np.asarray(y_test)
-
 print("Mean :", mean, "Std :", std)
 
-
+model = build_model()
 print(model.summary())
 # Compile
-from sklearn.metrics import f1_score
 opt = Adadelta()
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
 # Prepare fit
-from sklearn.utils import class_weight
 
 check = ModelCheckpoint("weights.{epoch:02d}-{val_acc:.5f}.hdf5", monitor='val_acc', verbose=0, save_best_only=True, save_weights_only=False, mode='auto')
 early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
-#cw = class_weight.compute_class_weight('balanced', np.unique(y_train.argmax(1)), y_train.argmax(1))
+#  cw = class_weight.compute_class_weight('balanced', np.unique(y_train.argmax(1)), y_train.argmax(1))
 cw = {i: (y_train.argmax(1) == i).sum() + (y_test.argmax(1) == i).sum() for i in range(12)}
 tot = sum([v for v in cw.values()])
-cw = {k: v / tot *100 for k, v in cw.items()}
+cw = {k: v / tot * 100 for k, v in cw.items()}
 print(cw)
 batch_size = 32
 
